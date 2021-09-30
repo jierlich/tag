33f16e66a53d0916f3eaab94473c927ca81a3ee4
jierlich <jierlich@umich.edu>
2021-09-30 01:15:56 -0400

    [TEST] test failure to mint without enough tag

--- test/tests.js
+++ test/tests.js
@@ -7,6 +7,7 @@ const BN = ethers.BigNumber.from
 // Note: The ether number can represent any 18 decimal token
 const oneEther = BN("1000000000000000000")
 const zero = BN("0")
+const fourTwenty = BN("420").mul(oneEther)
 
 describe("Tag game", () => {
     beforeEach(async () => {
@@ -21,7 +22,7 @@ describe("Tag game", () => {
         const ItFactory = await ethers.getContractFactory("It", deployer)
         this.it = await ItFactory.deploy("It", "IT", this.tag.address)
 
-        await this.tag.connect(deployer).mint(this.signers[1].address, BN('420').mul(oneEther))
+        await this.tag.connect(deployer).mint(this.signers[1].address, fourTwenty)
 
         // Give vault ERC20 minting permissions, revoke others from deployer
         const MINTER_ROLE = keccak256(toUtf8Bytes("MINTER_ROLE"))
@@ -41,9 +42,21 @@ describe("Tag game", () => {
     })
 
     it('burns Tag upon It mint', async () => {
-        expect(await this.tag.balanceOf(this.signers[1].address)).to.equal(BN('420').mul(oneEther))
+        expect(await this.tag.balanceOf(this.signers[1].address)).to.equal(fourTwenty)
         await this.it.connect(this.signers[1]).mint(this.signers[1].address)
         expect(await this.tag.balanceOf(this.signers[1].address)).to.equal(zero)
         expect(await this.it.ownerOf(1)).to.equal(this.signers[1].address)
+    })
+
+    it('fails to mint It without enough Tag', async () => {
+        await expect(
+            this.it.connect(this.signers[2]).mint(this.signers[1].address)
+        ).to.be.revertedWith('ERC20: burn amount exceeds balance')
+
+        await this.tag.connect(this.signers[1]).transfer(this.signers[2].address, fourTwenty)
+        expect(await this.tag.balanceOf(this.signers[2].address)).to.equal(fourTwenty)
+        await this.it.connect(this.signers[2]).mint(this.signers[2].address)
+        expect(await this.tag.balanceOf(this.signers[2].address)).to.equal(zero)
+        expect(await this.it.ownerOf(1)).to.equal(this.signers[2].address)
     })
 })
